66c4e440e173a1463534ecec798bad202ebf10da
yunkai1841 <70383706+yunkai1841@users.noreply.github.com>
2023-10-23 22:37:42 +0900

    [fix] save dir fixed

--- scripts/onnx_trt.sh
+++ scripts/onnx_trt.sh
@@ -2,6 +2,9 @@
 # Run the ONNX model with trtexec
 # precisions: fp32, fp16, int8, best
 
+result_dir="result/$1"
+trt_dir="temp/$1"
+
 # Check if the input argument is provided
 if [ -z "$1" ]
     then
@@ -15,47 +18,47 @@ if [ ! -f "$1" ]
 fi
 
 # Create the directories for the results
-mkdir -p result/fp32
-mkdir -p result/fp16
-mkdir -p result/int8
-mkdir -p result/best
-mkdir -p temp/fp32
-mkdir -p temp/fp16
-mkdir -p temp/int8
-mkdir -p temp/best
+mkdir -p ${result_dir}/fp32
+mkdir -p ${result_dir}/fp16
+mkdir -p ${result_dir}/int8
+mkdir -p ${result_dir}/best
+mkdir -p ${trt_dir}/fp32
+mkdir -p ${trt_dir}/fp16
+mkdir -p ${trt_dir}/int8
+mkdir -p ${trt_dir}/best
 
 # fp32
 # CUDA
-trtexec --onnx="$1" --saveEngine="temp/fp32/cuda.trt" \
-    --exportTimes="result/fp32/times_cuda.json" \
-    --exportOutput="result/fp32/output_cuda.json" \
-    --exportProfile="result/fp32/profile_cuda.json" \
-    --exportLayerInfo="result/fp32/layerinfo_cuda.json" \
-    > result/fp32/dump_cuda.txt
+trtexec --onnx="$1" --saveEngine="${trt_dir}/fp32/cuda.trt" \
+    --exportTimes="${result_dir}/fp32/times_cuda.json" \
+    --exportOutput="${result_dir}/fp32/output_cuda.json" \
+    --exportProfile="${result_dir}/fp32/profile_cuda.json" \
+    --exportLayerInfo="${result_dir}/fp32/layerinfo_cuda.json" \
+    > ${result_dir}/fp32/dump_cuda.txt
 # DLA
-trtexec --onnx="$1" --saveEngine="temp/fp32/dla.trt" \
+trtexec --onnx="$1" --saveEngine="${trt_dir}/fp32/dla.trt" \
     --allowGPUFallback --useDLACore=0 \
-    --exportTimes="result/fp32/times_dla.json" \
-    --exportOutput="result/fp32/output_dla.json" \
-    --exportProfile="result/fp32/profile_dla.json" \
-    --exportLayerInfo="result/fp32/layerinfo_dla.json" \
-    > result/fp32/dump_dla.txt
+    --exportTimes="${result_dir}/fp32/times_dla.json" \
+    --exportOutput="${result_dir}/fp32/output_dla.json" \
+    --exportProfile="${result_dir}/fp32/profile_dla.json" \
+    --exportLayerInfo="${result_dir}/fp32/layerinfo_dla.json" \
+    > ${result_dir}/fp32/dump_dla.txt
 
 for precision in fp16 int8 best
 do
     # CUDA
-    trtexec --onnx="$1" --saveEngine="temp/$precision/cuda.trt" --$precision \
-    --exportTimes="result/$precision/times_cuda.json" \
-    --exportOutput="result/$precision/output_cuda.json" \
-    --exportProfile="result/$precision/profile_cuda.json" \
-    --exportLayerInfo="result/$precision/layerinfo_cuda.json" \
-    > result/$precision/dump_cuda.txt
+    trtexec --onnx="$1" --saveEngine="${trt_dir}/$precision/cuda.trt" --$precision \
+    --exportTimes="${result_dir}/$precision/times_cuda.json" \
+    --exportOutput="${result_dir}/$precision/output_cuda.json" \
+    --exportProfile="${result_dir}/$precision/profile_cuda.json" \
+    --exportLayerInfo="${result_dir}/$precision/layerinfo_cuda.json" \
+    > ${result_dir}/$precision/dump_cuda.txt
     # DLA
-    trtexec --onnx="$1" --saveEngine="temp/$precision/dla.trt" --$precision \
+    trtexec --onnx="$1" --saveEngine="${trt_dir}/$precision/dla.trt" --$precision \
     --allowGPUFallback --useDLACore=0 \
-    --exportTimes="result/$precision/times_dla.json" \
-    --exportOutput="result/$precision/output_dla.json" \
-    --exportProfile="result/$precision/profile_dla.json" \
-    --exportLayerInfo="result/$precision/layerinfo_dla.json" \
-    > result/$precision/dump_dla.txt
+    --exportTimes="${result_dir}/$precision/times_dla.json" \
+    --exportOutput="${result_dir}/$precision/output_dla.json" \
+    --exportProfile="${result_dir}/$precision/profile_dla.json" \
+    --exportLayerInfo="${result_dir}/$precision/layerinfo_dla.json" \
+    > ${result_dir}/$precision/dump_dla.txt
 done
